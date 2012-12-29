00f81d5c96fcd7f98d8ac66cea817b7d3c40c7c8
micjohnson <micjohnson@gmail.com>
2012-12-29 13:49:17 -0800

    adding grow, updating docs

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ WeedPhp
 
 A PHP client for Weed-FS, a simple and highly scalable distributed file system.
 
+Weed-FS:
+http://code.google.com/p/weed-fs/
 
 Status:
 ======================
--- lib/WeedPhp/Client.php
+++ lib/WeedPhp/Client.php
@@ -3,90 +3,188 @@ namespace WeedPhp;
 
 use WeedPhp\Transport\Curl;
 
+/**
+ *
+ * Client for Weed-fs
+ *
+ * @author micjohnson
+ *
+ */
 class Client
 {
+    /** @var Curl transport */
     protected $transport;
 
+    /** @var string weed-fs master server prot://ip:port */
     protected $storageAddress;
 
+    /**
+     * 
+     * Create curl transport and set master server address
+     * 
+     * @param string $storageAddress
+     */
     public function __construct($storageAddress)
     {
         $this->transport = new Curl();
         $this->storageAddress = $storageAddress;
     }
 
-    public function assign($count)
+    /**
+     *
+     * Get a fid and a volume server url
+     *
+     * @param number $count
+     * @return mixed $response response from curl
+     */
+    public function assign($count, $replication = null)
     {
         $assignUrl = $this->storageAddress . '/dir/assign';
         $assignUrl .= '?count' . intval($count);
+        
+        if($replication !== null) {
+            $assignUrl .= '&replication=' . $replication;
+        }
+        
         $response = $assignUrl = $this->transport->get($assignUrl);
         $this->transport->close();
 
-        return $response;
-    }
-    
-    public function delete($storageVolumeAddress, $id)
-    {
-        $deleteUrl = $storageVolumeAddress . '/' . $id;
-        // TODO check for http://
-        $response = $this->transport->custom($deleteUrl, 'delete');
-        $this->transport->close();
-    
+        // {"count":1,"fid":"3,01637037d6","url":"127.0.0.1:8080","publicUrl":"localhost:8080"}
         return $response;
     }
 
+    /**
+     *
+     * Delete a file by fid on specified volume server
+     *
+     * @param string $storageVolumeAddress
+     * @param string $fid file id
+     * @return mixed $response
+     */
+    public function delete($volumeServerAddress, $fid)
+    {
+        $deleteUrl = $volumeServerAddress . '/' . $fid;
+        // TODO check for http://
+        $response = $this->transport->custom($deleteUrl, 'delete');
+        $this->transport->close();
+
+        return $response;
+    }
+
+    /**
+     *
+     * Lookup locations for specified volume by id
+     *
+     * @param number $volumeId
+     * @return mixed $response
+     */
     public function lookup($volumeId)
     {
         $lookupUrl = $this->storageAddress . '/dir/lookup';
         $lookupUrl .= '?volumeId=' . $volumeId;
         $response = $this->transport->get($lookupUrl);
         $this->transport->close();
-    
+
+        // {"locations":[{"publicUrl":"localhost:8080","url":"localhost:8080"}]}
         return $response;
     }
     
-    public function retrieve($storageVolumeAddress, $id)
+    /**
+     *
+     * This will assign $count volumes with $replication replication.
+     *
+     * for replication options see:
+     * http://code.google.com/p/weed-fs/#Rack-Aware_and_Data_Center-Aware_Replication
+     *
+     * @param number $count number of volumes
+     * @param string $replication something like 001
+     */
+    public function grow($count, $replication)
     {
-        $retrieveUrl = $storageVolumeAddress . '/' . $id;
+        $growUrl = $this->storageAddress . '/dir/grow';
+        $growUrl .= '?count=' . $count;
+        $growUrl .= '&replication=' . $replication;
+        $response = $this->transport->get($growUrl);
+
+        return $response;
+    }
+    
+    /**
+     *
+     * Retrieve a file from a specific volume server by fid
+     *
+     * @param string $volumeServerAddress
+     * @param string $fid
+     * @return mixed $response
+     */
+    public function retrieve($volumeServerAddress, $fid)
+    {
+        $retrieveUrl = $volumeServerAddress . '/' . $fid;
         // TODO check for http://
         $response = $this->transport->get($retrieveUrl);
         $this->transport->close();
 
         return $response;
     }
-    
+
+    /**
+     *
+     * Get information about volume's free space
+     *
+     */
     public function status()
     {
         $statusAddress = $this->storageAddress . '/dir/status';
         $response = $this->transport->get($statusAddress);
         $this->transport->close();
-    
+
         return $response;
     }
-    
-    public function storeMultiple($fileAddress, $id, array $files)
+
+    /**
+     * 
+     * Store multiple files at once, assuming you have assigned the same number of count for fid
+     * as you have number of files.
+     * 
+     * @param string $volumeServerAddress
+     * @param string $fid
+     * @param array $files
+     * @return mixed $response
+     */
+    public function storeMultiple($volumeServerAddress, $fid, array $files)
     {
         $count = count($files);
-        $storeUrl = $fileAddress . '/' . $id;
+        $storeUrl = $volumeServerAddress . '/' . $fid;
         // TODO check for http://
 
         $response = array();
         for($i = 1; $i <= $count; $i++) {
             $parameters = array('file'=>$files[$i-1]);
             $response[] = $this->transport->post($storeUrl, $parameters);
-            $storeUrl = $fileAddress . '/' . $id . '_' . $i;
+            $storeUrl = $volumeServerAddress . '/' . $fid . '_' . $i;
         }
         $this->transport->close();
 
         return $response;
     }
-    
-    public function store($fileAddress, $id, $file)
+
+    /**
+     * 
+     * Store a single file on volume server. Use assign first to get the volume server
+     * and fid
+     * 
+     * @param string $volumeServerAddress
+     * @param string $fid
+     * @param unknown $file
+     * @return mixed $response
+     */
+    public function store($volumeServerAddress, $fid, $file)
     {
-        $storeUrl = $fileAddress . 'id';
+        $storeUrl = $volumeServerAddress . '/' . $fid;
         $parameters = array('file'=>$file);
         $response = $this->transport->post(storeUrl, $parameters);
         $this->transport->close();
+
         return $response;
     }
 }
