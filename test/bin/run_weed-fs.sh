095bf2e1a2186c7ecc1ea8342119c65daae174a0
micjohnson <micjohnson@gmail.com>
2012-12-31 01:13:23 -0800

    readding waits, worth it

--- test/bin/run_weed-fs.sh
+++ test/bin/run_weed-fs.sh
@@ -6,9 +6,11 @@ mkdir /tmp/data1
 mkdir /tmp/data2
 ./weed-fs/weed master &
 echo "started master";
-sleep 1s;
+sleep 2s;
 ./weed-fs/weed volume -dir="/tmp/data1" -max=12  -mserver="localhost:9333" -port=8080 &
 echo "started volume 1: max=12";
+sleep 1s;
 ./weed-fs/weed volume -dir="/tmp/data2" -max=12  -mserver="localhost:9333" -port=8081 &
 echo "started volume 2: max=12";
+sleep 1s;
 echo "weed-fs master and volume servers running"
