b77ff0c5f53ae84c3b666208fffb2f47997521ee
micjohnson <micjohnson@gmail.com>
2012-12-31 01:10:27 -0800

    removing useless sleeps

--- test/bin/run_weed-fs.sh
+++ test/bin/run_weed-fs.sh
@@ -9,8 +9,6 @@ echo "started master";
 sleep 1s;
 ./weed-fs/weed volume -dir="/tmp/data1" -max=12  -mserver="localhost:9333" -port=8080 &
 echo "started volume 1: max=12";
-sleep 1s;
 ./weed-fs/weed volume -dir="/tmp/data2" -max=12  -mserver="localhost:9333" -port=8081 &
 echo "started volume 2: max=12";
-sleep 2s;
 echo "weed-fs master and volume servers running"
