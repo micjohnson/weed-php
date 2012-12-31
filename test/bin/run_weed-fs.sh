2c8e6ac92f7c57d6265926c0e4f8a65859100adf
micjohnson <micjohnson@gmail.com>
2012-12-30 18:56:11 -0800

    testing things

--- test/bin/run_weed-fs.sh
+++ test/bin/run_weed-fs.sh
@@ -4,7 +4,9 @@ tar -xzf weed-fs-0.26-linux-386.tar.gz
 echo "starting servers";
 mkdir /tmp/data1
 ./weed-fs/weed master &
+echo "started master";
+sleep 1s;
 ./weed-fs/weed volume -dir="/tmp/data1" -max=5  -mserver="localhost:9333" -port=8080 &
-sleep 5s
+echo "started volume";
+sleep 2s;
 echo "weed-fs master and volume server running"
-
