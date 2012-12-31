bbdaaa9a098edf361371330faa2c1941c3081f4f
micjohnson <micjohnson@gmail.com>
2012-12-31 02:36:25 +0000

    merging travis config

--- test/autoload.php
+++ test/autoload.php
@@ -1,10 +1,15 @@
 <?php
+defined('BASE_PATH') || define('BASE_PATH', realpath(dirname(__FILE__)));
 
-$path  = (string)get_include_path();
-$path .= (string)(PATH_SEPARATOR . '../lib');
+$path  = get_include_path() . PATH_SEPARATOR;
+$path .= BASE_PATH . '/../lib' . PATH_SEPARATOR;
+$path .= BASE_PATH . '/lib';
 
 set_include_path($path);
 
 spl_autoload_register(function ($className) {
-    include_once($className . '.php');
+    if(strpos(strtolower($className), "weedphp") !== false) {
+        $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+        include_once($className . '.php');
+    }
 });
