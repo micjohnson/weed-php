0d1b71ef603101c73c59907e620bf288bcab4d48
micjohnson <micjohnson@gmail.com>
2013-01-03 15:55:06 -0800

    updating Weed version

--- test/bin/run_weed-fs.sh
+++ test/bin/run_weed-fs.sh
@@ -4,13 +4,13 @@ tar -xzf weed-fs-latest-linux-386.tar.gz
 echo "starting servers";
 mkdir /tmp/data1
 mkdir /tmp/data2
-./weed-fs/weed master &
+./weed/weed master &
 echo "started master";
 sleep 2s;
-./weed-fs/weed volume -dir="/tmp/data1" -max=12  -mserver="localhost:9333" -port=8080 &
+./weed/weed volume -dir="/tmp/data1" -max=12  -mserver="localhost:9333" -port=8080 &
 echo "started volume 1: max=12";
 sleep 1s;
-./weed-fs/weed volume -dir="/tmp/data2" -max=12  -mserver="localhost:9333" -port=8081 &
+./weed/weed volume -dir="/tmp/data2" -max=12  -mserver="localhost:9333" -port=8081 &
 echo "started volume 2: max=12";
 sleep 1s;
 echo "weed-fs master and volume servers running"
